42c93cc0cb6eaaaeb9b28ed8bdb0b2e4f133ef76
Edusinx <edusinx@gmail.com>
2019-10-02 20:06:19 -0300

    andorinhas vacilão que não explica direito

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            for (c = 250; c <= 500; c++) 
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
